d3e17bcebf92e3feeb586fa420cca3afbcc90463
Prashant Bhate <Prashant.Bhate@monitise.com>
2017-06-23 14:51:52 +0100

    @WebMethod to controller

--- src/main/java/com/bhate/cet/allotment/MyWebMvcConfig.java
+++ src/main/java/com/bhate/cet/allotment/MyWebMvcConfig.java
@@ -8,15 +8,54 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MyWebMvcConfig {
 
-    @Bean
+	@Bean
 	public WebMvcConfigurerAdapter forwardToIndex() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName(
-                        "redirect:swagger-ui.html");
-            }
-        };
-    }
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addViewController("/")
+						.setViewName("redirect:swagger-ui.html");
+			}
+		};
+	}
+
+	//	@Bean
+	//	public WebMethodHandlerMapping webMethodHandler() {
+	//		return new WebMethodHandlerMapping();
+	//	}
 
 }
+
+//class WebMethodHandlerMapping extends RequestMappingHandlerMapping {
+//
+//	@Override
+//	protected boolean isHandler(Class<?> beanType) {
+//		return (AnnotatedElementUtils.hasAnnotation(beanType, WebService.class) || AnnotatedElementUtils.hasAnnotation(beanType, WebMethod.class));
+//	}
+//
+//	@Override
+//	protected RequestMappingInfo getMappingForMethod(Method method, Class<?> handlerType) {
+//		RequestMappingInfo info = createRequestMappingInfo(method);
+//		if (info != null) {
+//			RequestMappingInfo typeInfo = createRequestMappingInfo(handlerType);
+//			if (typeInfo != null) {
+//				info = typeInfo.combine(info);
+//			}
+//		}
+//		return info;
+//	}
+//
+//	private RequestMappingInfo createRequestMappingInfo(AnnotatedElement element) {
+//		WebMethod requestMapping = AnnotatedElementUtils.findMergedAnnotation(element, WebMethod.class);
+//		RequestCondition<?> condition = (element instanceof Class ? getCustomTypeCondition((Class<?>) element) : getCustomMethodCondition((Method) element));
+//		return (requestMapping != null ? createRequestMappingInfo(requestMapping, condition) : null);
+//	}
+//
+//	private RequestMappingInfo createRequestMappingInfo(WebMethod webMethod, RequestCondition<?> customCondition) {
+//		return RequestMappingInfo.paths(webMethod.operationName())
+//								 .methods(RequestMethod.POST)
+//								 .mappingName(webMethod.operationName())
+//								 .customCondition(customCondition)
+//								 .build();
+//	}
+//}
